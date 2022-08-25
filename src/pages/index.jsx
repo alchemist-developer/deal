e68d75658c6327910c36cf09b244568200ec7105
Adryel Simon <adryel@live.it>
2022-08-24 23:14:19 -0300

    build: base project | prettier eslint config

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,13 +1,13 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 const Heading = styled.h1`
-background-color: ${({ theme }) => theme.colors.secondaryColor};
-`
+  background-color: ${({ theme }) => theme.colors.secondaryColor};
+`;
 
 export default function Home() {
   return (
     <div>
       <Heading>Hello AutoForce</Heading>
     </div>
-  )
+  );
 }
