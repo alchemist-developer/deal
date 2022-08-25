3e1c6d0b21aebe0f73f57155f7d39ff7ec1cc3d0
Adryel Simon <adryel@live.it>
2022-08-25 03:39:29 -0300

    feat: box atomic component

---
+++ src/components/atoms/Box/index.jsx
@@ -0,0 +1,9 @@
+/* eslint-disable prettier/prettier */
+import * as S from "./styled";
+
+// eslint-disable-next-line react/prop-types
+const Box = ({ children }) => {
+  return <S.Box>{children}</S.Box>;
+};
+
+export default Box;
